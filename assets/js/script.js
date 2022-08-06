246f411ad4708ad267a8223358c12c9ddfaa1e29
Reece Devenney <reecedevenney@gmail.com>
2022-08-05 23:29:08 -0400

    clear start screeen text wtih start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 var question1 = {
     question: "flavor text",
     answers: ["Option 1", "Option 2", "Option 3", "Option 4"]
-}
+};
 
-var questionArray = [question1]
+var question2 = {
+    question: "flavor text",
+    answers: ["Option 2", "Option 2", "Option 3", "Option 4"]
+};
+
+var question3 = {
+    question: "flavor text",
+    answers: ["Option 3", "Option 2", "Option 3", "Option 4"]
+};
+
+var questionArray = [question1, question2, question3];
+
+var startEl = document.querySelector("#start-screen");
+
+var beginGame = function (event) {
+    var targetEl = event.target;
+
+    if (targetEl.matches(".start-btn")) {
+        startEl.style.display = "none";
+    };
+};
+
+startEl.addEventListener("click", beginGame);
