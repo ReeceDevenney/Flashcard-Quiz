a482c182d50e9f9798aead4cec8ff60883240a16
Reece Devenney <reecedevenney@gmail.com>
2022-08-06 19:32:51 -0400

    added end of game once all questions answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,8 @@ var questionArray = [question1, question2, question3];
 
 var startEl = document.querySelector("#start-screen");
 var gameEl = document.querySelector("#game-screen");
+var corIncorEl = document.querySelector("#correct-incorrect");
+var endGameEl = document.querySelector("#end-game")
 questionNumber = 0
 
 // removes the content of the start screen and calls the initializeGame function
@@ -33,11 +35,12 @@ var beginGame = function (event) {
 };
 // adds the html for the first question to appear
 var initializeGame = function () {
+    // fills in the p tag with the first question
     var questionEl = document.querySelector("#questions");
     questionEl.textContent = questionArray[questionNumber].question;
 
     var answersEl = document.querySelector("#answers");
-
+    // creates buttons that act as the 4 answer options
     for (var i = 0; i < questionArray[questionNumber].answers.length; i++) {
         var answerOptionEl = document.createElement("li");
         answerOptionEl.setAttribute("data-answer-id", i)
@@ -51,21 +54,42 @@ var initializeGame = function () {
 
 // function that plays the game by reacting to the answers that the player choses
 var playGame = function (event) {
-    var corIncorEl = document.querySelector("#correct-incorrect")
-    if (event.target.textContent === questionArray[questionNumber - 1].correct) {
-        corIncorEl.textContent = "✅ CORRECT"
+    if (questionNumber < questionArray.length) {
+        //checks for it the answer chosen was correct or incorrect
+        if (event.target.textContent === questionArray[questionNumber - 1].correct) {
+            corIncorEl.textContent = "✅ CORRECT"
+        } else {
+            corIncorEl.textContent = "❌ INCORRECT"
+        }
+        // adds the next questions to the p tag
+        var questionEl = document.querySelector("#questions");
+        questionEl.textContent = questionArray[questionNumber].question;
+        // adds the new answer options
+        for (var i = 0; i < questionArray[questionNumber].answers.length; i++) {
+            var updateAnswersEl = document.querySelector(".answer-btn[data-answer-id='" + i + "']");
+            updateAnswersEl.innerHTML = "<button class='answer-btn'>" + questionArray[questionNumber].answers[i]; + "</button>";
+        };
+        questionNumber++
     } else {
-        corIncorEl.textContent = "❌ INCORRECT"
-    }
-    var questionEl = document.querySelector("#questions");
-    questionEl.textContent = questionArray[questionNumber].question;
+        var questionEl = document.querySelector("#questions");
+        questionEl.textContent = "";
 
-    for (var i = 0; i < questionArray[questionNumber].answers.length; i++) {
-        var updateAnswersEl = document.querySelector(".answer-btn[data-answer-id='" + i + "']");
-        updateAnswersEl.innerHTML = "<button class='answer-btn'>" + questionArray[questionNumber].answers[i]; + "</button>";
-    };
-    questionNumber++
+        for (i = 0; i < 4; i++) {
+            var removeAnswerEl = document.querySelector(".answer-btn");
+            removeAnswerEl.remove();
+        };
+
+        corIncorEl.textContent = "";
+
+        var initialsEl = document.createElement("input");
+        initialsEl.textContent =
+        endGameEl.appendChild(initialsEl);
+        var submitInitialsEl = document.createElement("button");
+        submitInitialsEl.textContent = "submit"
+        endGameEl.appendChild(submitInitialsEl);
+    }
 }
+
 
 
 
